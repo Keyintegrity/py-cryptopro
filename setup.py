dfc9ecfb699caf14e00d0ff2b39fe70f4380abde
Artyom Opanchuk <artyom.opanchuk@gmail.com>
2017-08-23 15:48:37 +0300

    Добавил future в зависимости

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name='pycryptopro',
     version='0.0.8',
     description='',
+    install_requires=[
+        'future>=0.16.0',
+    ],
     url='https://github.com/uishnik/py-cryptopro',
     author='uishnik',
     license='MIT',
